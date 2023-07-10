2d9ec10b156c5a4473ee0a52d84e91650617f5ba
zatzk <zatzk@outlook.com>
2023-07-09 22:00:12 -0300

    chore: add cache manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ExchangeController } from './exchange/exchange.controller';
 import { HttpModule } from '@nestjs/axios';
 import { ExchangeService } from './exchange/exchange.service';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HttpModule],
+  imports: [ConfigModule.forRoot(), 
+    HttpModule,
+    CacheModule.register()
+    ],
   controllers: [ExchangeController],
   providers: [ExchangeService],
 })
